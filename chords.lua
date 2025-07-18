94a8ab963d0573cfa2857108f0dc2390a3517eb9
CMorooney <coolercal@gmail.com>
2025-07-17 20:53:17 -0400

    update for 0-5v input

--- chords.lua
+++ chords.lua
@@ -1,6 +1,7 @@
 --- chord selector
--- 1/v octave to akemi's chords (+ inversions) lookup
+-- use one CV to select the same chords for both Akemi's Castle and Sid's (and eventually Cizzle)
 
+-- below are the actual voltages used to address each chord in each module
 -- AKEMI'S :: CHORDS
   -- 0.0v - 1.5v > null/none
   -- 1.6v - 2.1v > 5ths
@@ -19,16 +20,29 @@
   -- 8.5v - 9.5v > detuned
   -- 9.6v - 10v  > detuned (2)
 
--- note: KSP starts at 1v (lowest c) when at middle (0) octave
+-- SID'S :: CHORDS
+  -- 0.0v - 0.5v > null/none
+  -- 0.5v - 1.0v > 5ths
+  -- 1.0v - 1.5v > Major
+  -- 1.5v - 2.0v > minor
+  -- 2.0v - 2.5v > suspended
+  -- 2.5v - 3.0v > augemented
+  -- 3.0v - 3.5v > diminished
+  -- 3.5v - 4.0v > octave (1)
+  -- 4.0v - 4.5v > octave (2)
+  -- 4.5v - 4.7v > detuned 
+  -- 4.7v - 5.0v > detuned (2)
 
-chord_map = {
+-- now we create lists of equal lengh for each module to sweep through for the single input cv
+CHORD_COUNT = 13
+akemi_chord_map = {
   0,   -- null
   1.8, -- 5ths
   3.9, -- Major
   4.4, -- minor
   4.9, -- suspended
   5.5, -- augmented
-  6.2, -- diminshed
+  6.2, -- diminished
   6.7, -- dominant 7
   7.0, -- Major 7
   7.8, -- minor 7
@@ -37,39 +51,58 @@ chord_map = {
   10   -- dissonant
 }
 
-chord_interval = 0.0833                     -- half step (1/12)
-first = 1.006358                            -- volts value of lowest note expected from controller
-last = #chord_map * interval + first        -- volts value of the highest note expected from the controller
+-- Sid's has a limited amount of chords, fallback to null/none where there is no comparable option to Akemi's
+sid_chord_map = {
+  0,   -- null
+  0.7, -- 5ths
+  1.2, -- Major
+  1.7, -- minor
+  2.2, -- suspended
+  2.7, -- augmented
+  0,   -- null
+  0,   -- null
+  0,   -- null
+  0,   -- null
+  0,   -- null
+  4.6, -- dissonant
+  4.9  -- dissonant
+}
+
+-- output constants
+OUTPUT_AKEMIS_CHORD = 3
+OUTPUT_SIDS_CHORD = 1
+OUTPUT_AKEMIS_INV = 4
+OUTPUT_SIDS_INV = 2
+
+-- we are assuming the range of the input voltage is 0-5v
+INPUT_MIN = 0
+INPUT_MAX = 5
+
+-- interval change required of input voltage to change chord
+INTERVAL = (INPUT_MAX - INPUT_MIN) / CHORD_COUNT
+-- amount to scale input voltage by in order to map it to output range
+SCALE = CHORD_COUNT / (INPUT_MAX-INPUT_MIN)
 
 function input_1_stream_update(v)
-  lastVal = 1   -- for window comparator
-  accessor = 1  -- keep track of target output to set when comparator catches
+  mapIndex = math.floor(v * SCALE); 
+  output[OUTPUT_AKEMIS_CHORD].volts = akemi_chord_map[mapIndex]
+  output[OUTPUT_SIDS_CHORD].volts = sid_chord_map[mapIndex]
 
-  for i = first,last,interval
-  do
-    if v >= lastVal and v < i and output[1].volts ~= chord_map[accessor]
-    then
-      output[1].volts = chord_map[accessor]
-    end
-
-    lastVal = i
-    accessor = accessor + 1
-  end
 end
 
--- AKEMI'S :: INVERSIONS
-  -- 0.00v - 1.21v > null/none
-  -- 1.22v - 2.51v > first
-  -- 2.52v - 3.81v > second
-  -- 3.82v - 5.00v > third
-
--- note: KSP 'mod' cv is going from ~0-5v
--- so -- no need for any script -- just plug directly in unless you want to change response curve somehow
+-- both Akemi's and Sid's already use 0-5v for chord inversions
+-- so we can just pass through
+function input_2_stream_update(v)
+  output[OUTPUT_AKEMIS_INV].volts = v;
+  output[OUTPUT_SIDS_INV].volts = v;
+end
 
 function init()
-    -- turn on 'stream' mode for input 1
+    -- turn on 'stream' mode for both inputs
     -- 0.001 sets the sample-rate of the input. 0.001s is 1ms or 1kHz
     -- this is the fastest supported stream rate 
     input[1].mode('stream', 0.001)
     input[1].stream = input_1_stream_update;
+    input[2].mode('stream', 0.001)
+    input[2].stream = input_2_stream_update;
 end
